baddde4eeb5f905b8c8076b92351599e3b335f0d
Oscar Flavio Baggini <flaviobaggini@gmail.com>
2022-09-10 19:12:32 -0300

    TDD

---
+++ test/clases/ProfesorTest.java
@@ -0,0 +1,17 @@
+package clases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
+class ProfesorTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		assertEquals(1, 1);
+	}
+
+}
